f4da0e8fb3d1790b74ec176e8c7031b6d1912810
Sreeram Vuppala <sreeramsaivuppala@gmail.com>
2026-03-14 17:59:39 -0400

    feat: created initial database schema

--- crates/terrier-server/migration/src/lib.rs
+++ crates/terrier-server/migration/src/lib.rs
@@ -2,15 +2,14 @@ pub use sea_orm_migration::prelude::*;
 
 mod m20220101_000001_create_table;
 mod m20260314_210918_create_users_table;
+mod m20260314_213956_create_initial_tables;
+mod m20260314_215609_create_initial_tables;
 
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![
-            Box::new(m20220101_000001_create_table::Migration),
-            Box::new(m20260314_210918_create_users_table::Migration),
-        ]
+        vec![Box::new(m20260314_213956_create_initial_tables::Migration)]
     }
 }
